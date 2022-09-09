79b0d00d9d3669d3997846ab5660016972fa4fe1
usausa9 <k021g1269@g.neec.ac.jp>
2022-09-09 14:49:15 +0900

    テスト

--- main.cpp
+++ main.cpp
@@ -5,7 +5,7 @@
 #include "WinApp.h"
 #include "AxisIndicator.h"
 #include "PrimitiveDrawer.h"
-
+ 
 // Windowsアプリでのエントリーポイント(main関数)
 int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 	WinApp* win = nullptr;
